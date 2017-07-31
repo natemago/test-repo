67293bb3d44b2ec64a7df0f4c6d3ffefe90d9c14
Pavle Jonoski <pavle.jonoski@keitaro.com>
2017-07-31 10:52:23 +0200

    Travis + cc config.

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+  "fmt"
+)
+
+func PrintSomeText(txt string) {
+  fmt.Println(txt)
+}
+
+func main() {
+  PrintSomeText("Hello World")
+}
---
+++ main_test.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+  "testing"
+)
+
+func TestPrintSomeText(t *testing.T) {
+  PrintSomeText("Unit test")
+}
